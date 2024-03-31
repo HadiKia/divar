a9702a20026542a1b27dfc76fd3654d3ce6d4c1f
HadiKia <hadikiamarzi@protonmail.com>
2024-03-31 19:51:52 +0330

    Configuring React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
 import Router from "router/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+
+import defaultOptions from "configs/reactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
