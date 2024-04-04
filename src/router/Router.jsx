6cbd3a5f4d4a89a5398165e29ca120702e4a3a3f
HadiKia <hadikiamarzi@protonmail.com>
2024-04-04 19:22:34 +0330

    Restricting user access to pages

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getProfile } from "services/user";
 import ReactLoading from "react-loading";
@@ -26,9 +26,25 @@ function Router() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/auth" element={<AuthPage />} />
-      <Route path="/admin" element={<AdminPage />} />
+      <Route
+        path="/dashboard"
+        element={data ? <DashboardPage /> : <Navigate to="/auth" />}
+      />
+      <Route
+        path="/auth"
+        element={data ? <Navigate to="/dashboard" /> : <AuthPage />}
+      />
+      <Route
+        path="/admin"
+        // admin number => 09189990099
+        element={
+          data && data.data.role === "ADMIN" ? (
+            <AdminPage />
+          ) : (
+            <Navigate to="/" />
+          )
+        }
+      />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
