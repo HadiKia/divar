006215d9e5e805c422992cc9c540f645d6f36a1f
HadiKia <hadikiamarzi@protonmail.com>
2024-04-28 15:46:51 +0330

    Adding the option to select the province when registering an advertisement

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -3,6 +3,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getCategory } from "services/admin";
 import { getCookie } from "utils/cookie";
 import { Listbox } from "@headlessui/react";
+import { provinces } from "constants/cities";
 import axios from "axios";
 import ReactLoading from "react-loading";
 import toast from "react-hot-toast";
@@ -39,6 +40,7 @@ function AddPost() {
   const [category, setCategory] = useState("");
   const [imageLabel, setImageLabel] = useState("عکس خود را بارگزاری کنید");
   const [isLoading, setIsLoading] = useState(false);
+  const [selectedProvince, setSelectedProvince] = useState(provinces[0]);
   const queryClient = useQueryClient();
 
   useEffect(() => {
@@ -57,27 +59,23 @@ function AddPost() {
       }
     }
   };
-
   const addHandler = (event) => {
     event.preventDefault();
 
     const formData = new FormData();
     for (let i in form) {
-      if (i === "category") {
-        formData.append("category", category._id);
-      } else {
-        formData.append(i, form[i]);
-      }
+      formData.append(i, form[i]);
     }
+    formData.set("category", category._id);
+    formData.set("city", selectedProvince.name);
 
     const token = getCookie("accessToken");
     if (
-      !form.title ||
-      !form.content ||
-      !form.amount ||
-      !form.amount ||
-      !form.city ||
-      !form.city
+      !formData.get("title") ||
+      !formData.get("content") ||
+      !formData.get("amount") ||
+      !formData.get("city") ||
+      !formData.get("category")
     )
       return toast.error("لطفا فرم را تکمیل کنید");
 
@@ -99,6 +97,7 @@ function AddPost() {
         setIsLoading(false);
       });
   };
+
   return (
     <form onChange={changeHandler} className="flex-1 md:max-w-xs">
       <h3 className={h3Style}>افزودن آگهی</h3>
@@ -143,16 +142,37 @@ function AddPost() {
       </div>
 
       <div className={inputBoxStyle}>
-        <input
-          type="text"
-          name="city"
-          id="city"
-          className="peer"
-          placeholder=""
-        />
-        <label htmlFor="city" className={labelStyle}>
-          شهر
-        </label>
+        <Listbox value={selectedProvince} onChange={setSelectedProvince}>
+          <Listbox.Label className={labelStyle}>استان</Listbox.Label>
+          <Listbox.Button className={listBoxButtonStyle}>
+            {selectedProvince.name}
+          </Listbox.Button>
+          <Listbox.Options className={listBoxOptionsStyle}>
+            {provinces.map((province, index) => (
+              <div key={province.id}>
+                <Listbox.Option
+                  key={province.id}
+                  value={province}
+                  disabled={province.unavailable}
+                  className={({ active }) =>
+                    `${listBoxOptionStyle} ${
+                      active ? listBoxOptionActiveStyle : null
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <span className={selected ? "text-primary" : ""}>
+                      {province.name}
+                    </span>
+                  )}
+                </Listbox.Option>
+                {index !== provinces.length - 1 && (
+                  <hr className="border-[#EDEDED]" />
+                )}
+              </div>
+            ))}
+          </Listbox.Options>
+        </Listbox>
       </div>
 
       <div className={inputBoxStyle}>
@@ -165,35 +185,40 @@ function AddPost() {
             {category.name}
           </Listbox.Button>
           <Listbox.Options className={listBoxOptionsStyle}>
-            {data?.data.map((item) => (
-              <Listbox.Option
-                key={item._id}
-                value={item}
-                className={({ active }) =>
-                  `${listBoxOptionStyle} ${
-                    active ? listBoxOptionActiveStyle : null
-                  }`
-                }
-              >
-                {({ selected }) => (
-                  <>
-                    <span
-                      className={`scale-[0.8] ${
-                        selected ? "text-primary" : ""
-                      }`}
-                    >
-                      <RenderIcon iconName={item.icon} />
-                    </span>
-                    <span
-                      className={`block truncate pt-0.5 ${
-                        selected ? "text-primary" : ""
-                      }`}
-                    >
-                      {item.name}
-                    </span>
-                  </>
+            {data?.data.map((item, index) => (
+              <div key={item._id}>
+                <Listbox.Option
+                  key={item._id}
+                  value={item}
+                  className={({ active }) =>
+                    `${listBoxOptionStyle} ${
+                      active ? listBoxOptionActiveStyle : null
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`scale-[0.8] ${
+                          selected ? "text-primary" : ""
+                        }`}
+                      >
+                        <RenderIcon iconName={item.icon} />
+                      </span>
+                      <span
+                        className={`block truncate pt-0.5 ${
+                          selected ? "text-primary" : ""
+                        }`}
+                      >
+                        {item.name}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+                {index !== item.length - 1 && (
+                  <hr className="border-[#EDEDED]" />
                 )}
-              </Listbox.Option>
+              </div>
             ))}
           </Listbox.Options>
         </Listbox>
@@ -205,7 +230,7 @@ function AddPost() {
           name="images"
           id="images"
           className={inputFileStyle}
-          accept="image/*"
+          accept="image/png, image/png"
         />
         <span className={inputFileNameStyle}>{imageLabel}</span>
         <label htmlFor="images" className={labelStyle}>
---
+++ src/constants/cities.js
@@ -0,0 +1,159 @@
+const provinces = [
+  {
+    id: 1,
+    name: "آذربایجان شرقی",
+    unavailable: false,
+  },
+  {
+    id: 2,
+    name: "آذربایجان غربی",
+    unavailable: false,
+  },
+  {
+    id: 3,
+    name: "اردبیل",
+    unavailable: false,
+  },
+  {
+    id: 4,
+    name: "اصفهان",
+    unavailable: false,
+  },
+  {
+    id: 5,
+    name: "ايلام",
+    unavailable: false,
+  },
+  {
+    id: 6,
+    name: "بوشهر",
+    unavailable: false,
+  },
+  {
+    id: 7,
+    name: "تهران",
+    unavailable: false,
+  },
+  {
+    id: 8,
+    name: "چهارمحال بختیاری",
+    unavailable: false,
+  },
+  {
+    id: 9,
+    name: "خراسان جنوبی",
+    unavailable: false,
+  },
+  {
+    id: 10,
+    name: "خراسان رضوی",
+    unavailable: false,
+  },
+  {
+    id: 11,
+    name: "خراسان شمالی",
+    unavailable: false,
+  },
+  {
+    id: 12,
+    name: "خوزستان",
+    unavailable: false,
+  },
+  {
+    id: 13,
+    name: "زنجان",
+    unavailable: false,
+  },
+  {
+    id: 14,
+    name: "سمنان",
+    unavailable: false,
+  },
+  {
+    id: 15,
+    name: "سيستان و بلوچستان",
+    unavailable: false,
+  },
+  {
+    id: 16,
+    name: "فارس",
+    unavailable: false,
+  },
+  {
+    id: 17,
+    name: "قزوين",
+    unavailable: false,
+  },
+  {
+    id: 18,
+    name: "قم",
+    unavailable: false,
+  },
+  {
+    id: 19,
+    name: "البرز",
+    unavailable: false,
+  },
+  {
+    id: 20,
+    name: "كردستان",
+    unavailable: false,
+  },
+  {
+    id: 21,
+    name: "کرمان",
+    unavailable: false,
+  },
+  {
+    id: 22,
+    name: "كرمانشاه",
+    unavailable: false,
+  },
+  {
+    id: 23,
+    name: "كهكيلويه و بويراحمد",
+    unavailable: false,
+  },
+  {
+    id: 24,
+    name: "گلستان",
+    unavailable: false,
+  },
+  {
+    id: 25,
+    name: "گيلان",
+    unavailable: false,
+  },
+  {
+    id: 26,
+    name: "لرستان",
+    unavailable: false,
+  },
+  {
+    id: 27,
+    name: "مازندران",
+    unavailable: false,
+  },
+  {
+    id: 28,
+    name: "مرکزی",
+    unavailable: false,
+  },
+  {
+    id: 29,
+    name: "هرمزگان",
+    unavailable: false,
+  },
+  {
+    id: 30,
+    name: "همدان",
+    unavailable: false,
+  },
+  {
+    id: 31,
+    name: "يزد",
+    unavailable: false,
+  },
+];
+
+export { provinces };
