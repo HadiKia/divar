b43d5bf1f89bb4e5ea4e1433a44ad5e48e44191a
HadiKia <hadikiamarzi@protonmail.com>
2024-05-05 17:32:04 +0330

    Loader updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
+import DivarIcon from "assets/icons/DivarIcon";
 import ReactLoading from "react-loading";
 
 function Loader() {
   return (
     <div className="fixed inset-0 bg-black/30 grid place-items-center z-30">
-      <div className="w-52 h-24 bg-white rounded-lg grid place-items-center">
+      <div className="w-52 pt-6 pb-4 bg-white rounded-lg grid place-items-center gap-y-4 text-primary ">
+        <span className="scale-[1.7]"><DivarIcon /></span>
         <ReactLoading
           type="bubbles"
           color="#A62626"
