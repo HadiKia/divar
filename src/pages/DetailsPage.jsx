36e8df1cfec6863ca568dcc2f5061cdfefef4e8f
HadiKia <hadikiamarzi@protonmail.com>
2024-05-08 23:04:13 +0330

    DetailsPage updated & styles added

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -5,6 +5,7 @@ import { getCategory } from "services/admin";
 
 import Loader from "components/Loader";
 import GalleryIcon from "assets/icons/GalleryIcon";
+import { sp } from "utils/numbers";
 
 function DetailsPage() {
   const baseURL = import.meta.env.VITE_BASE_URL;
@@ -13,35 +14,67 @@ function DetailsPage() {
 
   if (isLoading) return <Loader />;
 
-  const { category, createdAt, images, options, userMobile } = data.data.post;
+  const {
+    amount,
+    category,
+    createdAt,
+    images,
+    options,
+    userMobile,
+    updatedAt,
+  } = data.data.post;
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="container mx-auto max-w-screen-lg md:px-4 md:pt-20">
+      <div className="pb-28 flex flex-col md:flex-row-reverse md:items-start md:justify-between md:gap-x-10">
+        <div className="md:flex-1 md:flex md:justify-end ">
           {images.length ? (
-            <img src={`${baseURL}${images}`} />
+            <img
+              src={`${baseURL}${images}`}
+              className="w-full max-h-[340px] md:max-w-[400px] md:max-h-[380px] md:rounded"
+            />
           ) : (
-            <span>
+            <span className="w-full grid place-items-center bg-[#EBEBEB] text-secondary h-[340px] md:max-w-[400px] md:h-[380px] md:rounded">
               <GalleryIcon />
             </span>
           )}
         </div>
 
-        <div>
+        <div className="px-4 md:px-0 md:flex-1">
           <RenderCategory categoryName={category} />
-          <h2>{options.title}</h2>
-          <div>
+          <h2 className="text-dark text-xl font-medium mb-2.5">
+            {options.title}
+          </h2>
+          <div className="flex items-center justify-between text-sm text-secondary mb-7">
             <span>{options.city}</span>
             <span> {new Date(createdAt).toLocaleDateString("fa-IR")}</span>
           </div>
-          <h3>توضیحات:</h3>
-          <p>{options.content}</p>
-        </div>
+          <div className="flex flex-col-reverse">
+            <div>
+              {category !== "66344014638cf78dff8677d6" && (
+                <div className="flex items-center justify-between text-dark py-3 border-t">
+                  <span className="text-secondary">قیمت</span>
+                  <span>{sp(amount)} تومان</span>
+                </div>
+              )}
 
-        <button>
-          <a href={`tel:${userMobile}`}>اطلاعات تماس</a>{" "}
-        </button>
+              <div className="flex items-center justify-between text-dark py-3 mb-6 border-y">
+                <span className="text-secondary">آخرین به‌روزرسانی آگهی</span>
+                <span>{new Date(updatedAt).toLocaleDateString("fa-IR")}</span>
+              </div>
+              <h3 className="text-dark text-lg font-medium mb-1.5">توضیحات</h3>
+              <p className="text-dark text-sm leading-7">{options.content}</p>
+            </div>
+            <button className="fixed left-0 right-0 bottom-0 h-[64px] border-t z-20 bg-white px-4 py-3 md:relative md:border-t-0 md:w-full md:px-0 md:mb-5">
+              <a
+                href={`tel:${userMobile}`}
+                className="h-full flex items-center justify-center rounded bg-primary text-white font-medium"
+              >
+                اطلاعات تماس
+              </a>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -53,5 +86,9 @@ function RenderCategory({ categoryName }) {
   const { data } = useQuery(["get-categories"], getCategory);
   const categoryData = data?.data.find((cat) => cat._id === categoryName);
 
-  return <div>دسته بندی : {categoryData?.name}</div>;
+  return (
+    <div className="py-6 text-xs text-secondary md:hidden">
+      دسته بندی : {categoryData?.name}
+    </div>
+  );
 }
